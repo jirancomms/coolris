969bb65b73be40759043c7915da26231492015f7
jirancomms <coolweb@jiran.com>
2019-02-08 17:16:24 +0900

    first import

---
+++ README.md
@@ -0,0 +1,32 @@
+# 쿨스쿨 연계 서비스(coolschool relationship service, coolris-service)
+
+## 필요한 프레임워크
+- 구글 폰트(나눔고딕)
+- jquery 최소 v1.12.4
+- popper v1.14.6
+- bootstrap v4.2.1
+
+## 주의사항
+- 스크립트의 순서가 중요하다
+    1. jquery.js
+    2. popper.js
+    3. bootstrap.js
+    4. coolris.js
+- 포로토콜에 맞게 스크립트를 삽입한다. (http -> http, https -> https)
+
+## 사용방법
+- css, js파일을 포함시킨다. (jquery, popper, bootstrap, coolris)
+```html
+<link href="https://fonts.googleapis.com/css?family=Nanum+Gothic" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/bootstrap.min.css" /><!-- 4.1.1-->
+<script type="text/javascript" src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/jquery.min.js"></script><!-- 1.12.4 -->
+<script type="text/javascript" src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/popper.min.js"></script>
+<script type="text/javascript" src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/bootstrap.min.js"></script><!-- 4.1.1-->
+<script type="text/javascript" src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolris.js"></script>
+```
+- 쿨서비스 팝업이 들어갈 div를 **id를 coolris**로 만들어준다.
+```html
+<div id="coolris"></div>
+```
+## 예제
+- index.html 참고
---
+++ coolris.ts
@@ -0,0 +1,141 @@
+declare var $: any;
+declare var ga: any;
+
+class Coolris {
+    constructor() {
+    }
+
+    start(template) {
+        $('#coolris').html(template);
+    }
+
+    template() {
+        return `
+        <style>
+            .dropdown[coolris] .dropdown-toggle {
+                appearance: none !important;
+                -moz-appearance: none !important; /* Firefox */
+                -webkit-appearance: none !important; /* Safari and Chrome */
+                cursor: pointer !important;
+                background-color: #ffffff !important;
+            }
+            .dropdown[coolris] .dropdown-toggle:after {
+                display: none !important;
+            }
+            .dropdown[coolris] .dropdown-menu {
+                border-color: #b2b2b2 !important;
+                min-width: 285px !important;
+            }   
+            .dropdown[coolris] .dropdown-menu .bottom {
+                background-color: #fbfbfb !important;
+            }   
+            .dropdown[coolris] .dropdown-menu a:hover {
+                text-decoration: none !important;
+            }   
+            .dropdown[coolris] .dropdown-menu a {
+                margin-top: 2px !important;
+            }   
+            .dropdown[coolris] .dropdown-menu span, .dropdown[coolris] .dropdown-menu p {
+                color: #000000 !important;
+                font-size: 14px !important;
+                line-height: 36px !important;
+                font-weight: bold !important;
+                font-family: 'Nanum Gothic', sans-serif;
+            }   
+            .dropdown[coolris] .dropdown-menu p {
+                line-height: 28px !important;
+            }   
+        </style>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+            ga('create', 'UA-119402731-1', 'auto');
+            ga('send', 'pageview');
+            
+        </script>
+        <div class="dropdown" coolris>
+            <span class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolservice.png" alt="쿨스쿨 서비스 버튼">
+            </span>
+            <div class="dropdown-menu pt-2" aria-labelledby="dropdownMenuButton">
+                <p class="pl-4 pr-4 mb-0">쿨스쿨 아이디 하나로 이용해 보세요!</p>
+                <div class="dropdown-divider"></div>
+                <div class="pl-4 pr-4 clearfix">
+                    <a onclick="ga('send','event','link','click', 'coolris-coolschool')" class="col-6 float-left p-0" href="http://www.coolschool.co.kr/" target="_blank">
+                        <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolschool.png" alt="쿨스쿨" />
+                        <span class="align-middle">쿨스쿨</span>
+                    </a>
+                    <a onclick="ga('send','event','link','click', 'coolris-samstory')" class="col-6 float-left p-0" href="http://samstory.coolschool.co.kr" target="_blank">
+                        <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/samstory.png" alt="샘스토리" />
+                        <span class="align-middle">샘스토리</span>
+                    </a>
+                </div>
+                <div class="pl-4 pr-4 clearfix">
+                    <a onclick="ga('send','event','link','click', 'coolris-coolbooks')" class="col-6 float-left p-0" href="https://coolbooks.coolschool.co.kr/" target="_blank">
+                        <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolbooks.png" alt="쿨북스" />
+                        <span class="align-middle">쿨북스</span>
+                    </a>
+                    <a onclick="ga('send','event','link','click', 'coolris-ourschoolBook')" class="col-6 float-left p-0" href="https://school.coolbooks.co.kr/" target="_blank">
+                        <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/our-library.png" alt="우리학교서재" />
+                        <span class="align-middle">우리학교서재</span>
+                    </a>
+                </div>
+                <div class="pl-4 pr-4 clearfix">
+                    <a onclick="ga('send','event','link','click', 'coolris-weteacher')" class="col-6 float-left p-0" href="http://weteacher.coolschool.co.kr/" target="_blank">
+                        <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/weteacher.png" alt="위티쳐몰" />
+                        <span class="align-middle">위티쳐몰</span>
+                    </a>
+                    <a onclick="ga('send','event','link','click', 'coolris-coolissue')" class="col-6 float-left p-0" href="http://issue.coolschool.co.kr" target="_blank">
+                        <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolissue.png" alt="쿨이슈" />
+                        <span class="align-middle">쿨이슈</span>
+                    </a>
+                </div>
+                <div class="pl-4 pr-4 clearfix">                    
+                    <a onclick="ga('send','event','link','click', 'coolris-coolmessenger')" class="col-6 float-left p-0" href="http://school.coolmessenger.com/" target="_blank">
+                        <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolmessenger.png" alt="쿨메신저" />
+                        <span class="align-middle">쿨메신저</span>
+                    </a>
+                </div>
+                <div class="dropdown-divider mb-0"></div>
+                <div class="bottom pb-3 pt-2">
+                    <div class="pl-4 pr-4 clearfix">
+                        <a onclick="ga('send','event','link','click', 'coolris-coolview')" class="col-6 float-left p-0" href="https://coolview.coolschool.co.kr" target="_blank">
+                            <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolview.png" alt="쿨뷰" />
+                            <span class="align-middle">쿨뷰</span>
+                        </a>
+                        <a onclick="ga('send','event','link','click', 'coolris-coolnote')" class="col-6 float-left p-0" href="http://coolnote.coolschool.co.kr/" target="_blank">
+                            <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolnote.png" alt="쿨노트" />
+                            <span class="align-middle">쿨노트</span>
+                        </a>
+                    </div>
+                    <div class="pl-4 pr-4 clearfix">
+                        <a onclick="ga('send','event','link','click', 'coolris-coolmova')" class="col-6 float-left p-0" href="http://coolmova.coolschool.co.kr/" target="_blank">
+                            <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolmova.png" alt="쿨모바" />
+                            <span class="align-middle">쿨모바</span>
+                        </a>
+                        <a onclick="ga('send','event','link','click', 'coolris-coolsms')" class="col-6 float-left p-0" href="https://coolsms.coolmessenger.com" target="_blank">
+                            <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolsms.png" alt="쿨SMS" />
+                            <span class="align-middle">쿨SMS</span>
+                        </a>
+                    </div>
+                    <div class="pl-4 pr-4 clearfix">
+                        <a onclick="ga('send','event','link','click', 'coolris-timplus')" class="col-6 float-left p-0" href="http://timplus.coolschool.co.kr/" target="_blank">
+                            <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/teamplus.png" alt="팀플러스" />
+                            <span class="align-middle">팀플러스</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+}
+
+$(() => {
+    var coolris = new Coolris();
+    var template = coolris.template();
+    coolris.start(template);
+});
