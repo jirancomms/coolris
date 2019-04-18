585f6542f480dfc985007370199ff025281a9cc1
asd0905 <asd0905@jiran.com>
2019-04-18 14:33:36 +0900

    구글코드를 빼는 작업 - 원래 심겨져있던 구글코드를 빼서 따로 집어넣어야하도록 수정

--- README.md
+++ README.md
@@ -16,7 +16,8 @@
     1. jquery.js
     2. popper.js
     3. bootstrap.js
-    4. coolris.js
+    4. 필요한 구글코드 (예시: 쿨스쿨의 구글코드)
+    5.. coolris.js
 - 포로토콜에 맞게 스크립트를 삽입한다. (http -> http, https -> https)
 
 ## 사용방법
@@ -26,6 +27,15 @@
 <script type="text/javascript" src="//update.coolmessenger.com/_ImageServer/coolschool/commonTop/jquery.min.js"></script><!-- 1.12.4 -->
 <script type="text/javascript" src="//update.coolmessenger.com/_ImageServer/coolschool/commonTop/popper.min.js"></script>
 <script type="text/javascript" src="//update.coolmessenger.com/_ImageServer/coolschool/commonTop/bootstrap.min.js"></script><!-- 4.1.1-->
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-119402731-1', 'auto');
+    ga('send', 'pageview');
+</script>
 <script type="text/javascript" src="//update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolris.js"></script>
 ```
 - 쿨서비스 팝업이 들어갈 div를 **id를 coolris**로 만들어준다.
--- coolris.ts
+++ coolris.ts
@@ -120,31 +120,21 @@ class Coolris {
                 margin-right: 1px;
             }   
         </style>
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','coolris_ga');
-    
-            coolris_ga('create', 'UA-119402731-1', 'auto');
-            coolris_ga('send', 'pageview');
-            
-        </script>
         <div class="dropdown" coolris>
-            <span onclick="coolris_ga('send','event','link','click', 'coolris-coolfamily')" class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span onclick="ga('send','event','link','click', 'coolris-coolfamily')" class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolservice.png" alt="쿨스쿨 서비스 버튼">
             </span>
             <div class="dropdown-menu pt-2" aria-labelledby="dropdownMenuButton">
                 <p class="pl-4 pr-4 mb-0">쿨스쿨 아이디 하나로 이용해 보세요!</p>
                 <div class="dropdown-divider"></div>
                 <div class="pl-4 pr-4 clearfix">
-                    <a onclick="coolris_ga('send','event','link','click', 'coolris-coolschool')" class="col-6 float-left p-0" href="http://www.coolschool.co.kr/">
+                    <a onclick="ga('send','event','link','click', 'coolris-coolschool')" class="col-6 float-left p-0" href="http://www.coolschool.co.kr/">
                         <span class="img">
                             <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolschool.png" alt="쿨스쿨" />
                         </span>
                         <span class="align-middle">쿨스쿨</span>
                     </a>
-                    <a onclick="coolris_ga('send','event','link','click', 'coolris-samstory')" class="col-6 float-left p-0" href="http://samstory.coolschool.co.kr">
+                    <a onclick="ga('send','event','link','click', 'coolris-samstory')" class="col-6 float-left p-0" href="http://samstory.coolschool.co.kr">
                         <span class="img">
                             <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/samstory.png" alt="샘스토리" />
                         </span>
@@ -152,13 +142,13 @@ class Coolris {
                     </a>
                 </div>
                 <div class="pl-4 pr-4 clearfix">
-                    <a onclick="coolris_ga('send','event','link','click', 'coolris-coolbooks')" class="col-6 float-left p-0" href="https://coolbooks.coolschool.co.kr/">
+                    <a onclick="ga('send','event','link','click', 'coolris-coolbooks')" class="col-6 float-left p-0" href="https://coolbooks.coolschool.co.kr/">
                         <span class="img">
                             <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolbooks.png" alt="쿨북스" />
                         </span>
                         <span class="align-middle">쿨북스</span>
                     </a>
-                    <a onclick="coolris_ga('send','event','link','click', 'coolris-ourschoolBook')" class="col-6 float-left p-0" href="https://school.coolbooks.co.kr/">
+                    <a onclick="ga('send','event','link','click', 'coolris-ourschoolBook')" class="col-6 float-left p-0" href="https://school.coolbooks.co.kr/">
                         <span class="img">
                             <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/our-library.png" alt="우리학교서재" />
                         </span>
@@ -166,13 +156,13 @@ class Coolris {
                     </a>
                 </div>
                 <div class="pl-4 pr-4 clearfix">
-                    <a onclick="coolris_ga('send','event','link','click', 'coolris-weteacher')" class="col-6 float-left p-0" href="http://weteacher.coolschool.co.kr/">
+                    <a onclick="ga('send','event','link','click', 'coolris-weteacher')" class="col-6 float-left p-0" href="http://weteacher.coolschool.co.kr/">
                         <span class="img">
                             <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/weteacher.png" alt="WeTeacher" />
                         </span>
                         <span class="align-middle">WeTeacher</span>
                     </a>
-                    <a onclick="coolris_ga('send','event','link','click', 'coolris-coolissue')" class="col-6 float-left p-0" href="http://issue.coolschool.co.kr">
+                    <a onclick="ga('send','event','link','click', 'coolris-coolissue')" class="col-6 float-left p-0" href="http://issue.coolschool.co.kr">
                         <span class="img">
                             <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolissue.png" alt="쿨이슈" />
                         </span>
@@ -180,13 +170,13 @@ class Coolris {
                     </a>
                 </div>
                 <div class="pl-4 pr-4 clearfix">                    
-                    <a onclick="coolris_ga('send','event','link','click', 'coolris-coolmessenger')" class="col-6 float-left p-0" href="http://school.coolmessenger.com/">
+                    <a onclick="ga('send','event','link','click', 'coolris-coolmessenger')" class="col-6 float-left p-0" href="http://school.coolmessenger.com/">
                         <span class="img">
                             <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolmessenger.png" alt="쿨메신저" />
                         </span>
                         <span class="align-middle">쿨메신저</span>
                     </a>
-                    <a onclick="coolris_ga('send','event','link','click', 'coolris-cooledu')" class="col-6 float-left p-0" href="http://cooledu.coolschool.co.kr">
+                    <a onclick="ga('send','event','link','click', 'coolris-cooledu')" class="col-6 float-left p-0" href="http://cooledu.coolschool.co.kr">
                         <span class="img">
                             <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/cooledu.png" alt="통합연수" />
                         </span>
@@ -196,13 +186,13 @@ class Coolris {
                 <div class="dropdown-divider mb-0"></div>
                 <div class="bottom pb-3 pt-2">
                     <div class="pl-4 pr-4 clearfix">
-                        <a onclick="coolris_ga('send','event','link','click', 'coolris-coolview')" class="col-6 float-left p-0" href="https://coolview.coolschool.co.kr">
+                        <a onclick="ga('send','event','link','click', 'coolris-coolview')" class="col-6 float-left p-0" href="https://coolview.coolschool.co.kr">
                             <span class="img">
                                 <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolview.png" alt="쿨뷰" />
                             </span>
                             <span class="align-middle">쿨뷰</span>
                         </a>
-                        <a onclick="coolris_ga('send','event','link','click', 'coolris-coolnote')" class="col-6 float-left p-0" href="http://coolnote.coolschool.co.kr/">
+                        <a onclick="ga('send','event','link','click', 'coolris-coolnote')" class="col-6 float-left p-0" href="http://coolnote.coolschool.co.kr/">
                             <span class="img">
                                 <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolnote.png" alt="쿨노트" />
                             </span>
@@ -210,13 +200,13 @@ class Coolris {
                         </a>
                     </div>
                     <div class="pl-4 pr-4 clearfix">
-                        <a onclick="coolris_ga('send','event','link','click', 'coolris-coolmova')" class="col-6 float-left p-0" href="http://coolmova.coolschool.co.kr/">
+                        <a onclick="ga('send','event','link','click', 'coolris-coolmova')" class="col-6 float-left p-0" href="http://coolmova.coolschool.co.kr/">
                             <span class="img">
                                 <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolmova.png" alt="쿨모바" />
                             </span>
                             <span class="align-middle">쿨모바</span>
                         </a>
-                        <a onclick="coolris_ga('send','event','link','click', 'coolris-coolsms')" class="col-6 float-left p-0" href="https://coolsms.coolmessenger.com">
+                        <a onclick="ga('send','event','link','click', 'coolris-coolsms')" class="col-6 float-left p-0" href="https://coolsms.coolmessenger.com">
                             <span class="img">
                                 <img src="http://update.coolmessenger.com/_ImageServer/coolschool/commonTop/coolsms.png" alt="쿨SMS" />
                             </span>
@@ -319,7 +309,7 @@ class Coolris {
                 }
             </style>
             <div class="dropdown" coolProfile>
-            <span onclick="coolris_ga('send','event','link','click', 'coolProfile-profile')" class="dropdown-toggle" type="button" id="coolProfileDropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span onclick="ga('send','event','link','click', 'coolProfile-profile')" class="dropdown-toggle" type="button" id="coolProfileDropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <img class="profile_img" src="http://update.coolmessenger.com/_ImageServer/coolschool/resources/images/default_profile.png" alt="default_profile" />
                 <span class="profie_name">김새미</span><img class="coolprofile_arrow" src="http://update.coolmessenger.com/_ImageServer/coolschool/resources/images/coolprofile_arrow.png" alt="coolprofile_arrow" />
             </span>
@@ -328,12 +318,12 @@ class Coolris {
                 <p>내정보 보기</p>
                 <div>
                     내 쿨포인트
-                    <a onclick="coolris_ga('send','event','link','click', 'coolProfile-coolpoint')" href="//point.coolschool.co.kr" target="_blank">
+                    <a onclick="ga('send','event','link','click', 'coolProfile-coolpoint')" href="//point.coolschool.co.kr" target="_blank">
                         <img src="http://update.coolmessenger.com/_ImageServer/coolschool/resources/images/point.png" alt="point" />   
                         1,345원         
                     </a>
                 </div>
-                <span onclick="coolris_ga('send','event','link','click', 'coolProfile-logout')" class="logout">로그아웃</span>
+                <span onclick="ga('send','event','link','click', 'coolProfile-logout')" class="logout">로그아웃</span>
             </div>
         </div>
         `
