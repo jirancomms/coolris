85f124c1185730cff7e9f476665bb7bf29d59c76
정용채 <asd0905@naver.com>
2022-08-10 16:37:26 +0900

    컴즈리스 > 구글 애널리틱스 버전 업데이트
    컴즈리스 > 구글 애널리틱스 버전 업데이트

--- commsris-1.0.0.ts
+++ commsris-1.0.0.ts
@@ -11,7 +11,7 @@ export class Commsris extends Risbase {
 
     async start(coolrisOpt: CoolrisOpts | any = undefined): Promise<void> {
         const options: any = this.initOption(coolrisOpt);
-        options.gaOpts.gaMeasurementId = options.gaOpts.gaMeasurementId ? options.gaOpts.gaMeasurementId : 'UA-92421532-1';
+        options.gaOpts.gaMeasurementId = options.gaOpts.gaMeasurementId ? options.gaOpts.gaMeasurementId : 'G-6F5MJCTM7J';
         await super.start(options);
     }
 
@@ -28,7 +28,7 @@ export class Commsris extends Risbase {
 
     footerStart(coolrisOpt: CoolrisOpts | any = undefined) {
         const options: any = this.initOption(coolrisOpt);
-        options.gaOpts.gaMeasurementId = options.gaOpts.gaMeasurementId ? options.gaOpts.gaMeasurementId : 'UA-92421532-1';
+        options.gaOpts.gaMeasurementId = options.gaOpts.gaMeasurementId ? options.gaOpts.gaMeasurementId : 'G-6F5MJCTM7J';
         this.setGoogleCode(options); // 구글 ID적용, 구글 코드 함수 적용
 
         // interface footerTemplate에 템플릿 주기
@@ -72,5 +72,19 @@ export class Commsris extends Risbase {
             type: 'GET'
         };
         return $.ajax(setting).then(response => {return response.data});
+    }
+
+    /**
+     * 구글코드 옵션 합쳐주기 (디폴트 옵션 주기)
+     * @param coolrisOpt
+     */
+    initOption(coolrisOpt: CoolrisOpts | any = undefined): CoolrisOpts {
+        const defOption: CoolrisOpts = {
+            gaOpts: {
+                gaPageviewCall: true,
+                gaMeasurementId: 'G-6F5MJCTM7J'
+            }
+        } as any;
+        return $.extend(true, defOption, coolrisOpt);
     }
 }
