a42450cd7a2102092c0f96475d63b2d61218e48c
jirancomms <coolweb@jiran.com>
2024-04-05 12:09:27 +0900

    chore: 대표자명변경
    회사 대표자명 변경

--- service/commsris-service.ts
+++ service/commsris-service.ts
@@ -522,7 +522,7 @@ export class CommsrisService extends RisService {
                             <img src="//update.coolmessenger.com/_ImageServer/coolris/ico_youtube_big.png" alt="img" />
                         </a>            
                     </div>
-                    <p><span>대표자 : 황찬우</span><span>사업자등록번호 : 764-87-00314</span><span>통신판매업 신고번호 : 제2023-성남수정-0685호</span></p>
+                    <p><span>대표자 : 안대근</span><span>사업자등록번호 : 764-87-00314</span><span>통신판매업 신고번호 : 제2023-성남수정-0685호</span></p>
                     <p><span>(13453) 경기도 성남시 수정구 금토로80번길 37, 인피니티타워 W동 10층</span><span>대표전화 : <a href="tel:1670-4846">1670-4846</a></span><span>팩스 : 070-7605-2935</span></p>
                     <p><span>개인정보관리책임자 : 박노준</span>
                     <span style="cursor: pointer;" onClick="gtag('event', '${serviceName}', {'event_category': 'link', 'event_label': 'privacy_policy', 'send_to': '${gaMeasurementId}'});
@@ -536,7 +536,7 @@ export class CommsrisService extends RisService {
                     <div class="text">
                         <p>
                             <span>(주) 지란지교컴즈</span>
-                            <span class="mobile-span">대표자 : 황찬우</span>
+                            <span class="mobile-span">대표자 : 안대근</span>
                         </p>
                         <p><span class="mobile-span">사업자등록번호 : 764-87-00314</span></p>
                         <p><span class="mobile-span">통신판매업 신고번호 : 제2023-성남수정-0685호</span></p>
